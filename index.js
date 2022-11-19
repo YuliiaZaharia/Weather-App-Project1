0e2214e7e757357d47b1fefd0876492a1b3c9b42
YuliiaZaharia <juliarybalchenko999@gmail.com>
2022-11-19 21:04:37 +0300

    created function formatDay in JS

--- index.js
+++ index.js
@@ -19,9 +19,16 @@ function formatDate(date) {
       "Saturday"
     ];
     let day = days[dayIndex];
-  
     return `${day} ${hours}:${minutes}`;
   }
+
+ function formatDay (timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+ }
 
   function displayForecast(response) {
   let forecast = response.data.daily;
